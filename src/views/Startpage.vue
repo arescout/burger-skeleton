7bdc0172c68a04c14e07f46348cdb1c93e60767d
MrFantasio <henry.sjogren.0184@student.uu.se>
2019-12-06 12:46:27 +0100

    fixad front page

--- src/views/Startpage.vue
+++ src/views/Startpage.vue
@@ -1,5 +1,9 @@
 <template>
+    <div class = "IntroText">
+        <h1>Welcome to Crafy Burgers</h1>
+
     <div class="welcomeText">Här är en hejtext</div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +13,7 @@
 </script>
 
 <style scoped>
-
+    .IntroText {
+        background-image: url("https://i.pinimg.com/originals/ec/7c/99/ec7c9939f38e474f1050d59e0fc87a95.jpg");
+    }
 </style>
