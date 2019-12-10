b0af30d39cfce2d78551c44671cbe5b31622d873
MrFantasio <henry.sjogren.0184@student.uu.se>
2019-12-10 10:44:08 +0100

    changes made to button on startpage, tried making the whole button clickable...

--- src/views/Startpage.vue
+++ src/views/Startpage.vue
@@ -1,8 +1,17 @@
 <template>
     <div class = "IntroText">
     <div class="welcomeText">Welcome to Indie Burgers</div>
-    <button id = "firstButton"><router-link to="/">Go to orderings</router-link></button>
+
+        <section class = "welcomeSection">
+            <div>
+                <h1>Welcome to Krafty Burgers</h1>
+                We are a restaurant dedicated to providing exactly the burger that you want<br>
+                Here, we love cows <3
+                <button id = "firstButton"><router-link to="/">Go to orderings</router-link></button>
+            </div>
+        </section>
     </div>
+
 </template>
 
 <script>
@@ -17,6 +26,8 @@
         font-size: 3em;
         font-family: ".SF NS Display", "sans-serif";
         padding-top: 5em;
+        padding-bottom: 1em;
+
     }
     .IntroText {
         background-image: url("https://i.pinimg.com/originals/ec/7c/99/ec7c9939f38e474f1050d59e0fc87a95.jpg");
@@ -24,12 +35,35 @@
         background-size: cover;
         height: 50em;
         border: 5px solid black;
+        padding-left: 20em;
+        padding-right: 20em;
+
+    }
+    .welcomeSection {
+        text-align: center;
+        border: black solid 5px;
+        color: black;
+        background-color: bisque;
+        border-radius: 50px;
     }
     #firstButton {
         text-align: center;
+        font-size: 15px;
+        border-radius: 40%;
+        border: 2px solid black;
+        padding: 15px 20px;
         margin-top: 5%;
-        margin-left: 45%;
+        margin-left: 42%;
         margin-right: 45%;
+        margin-bottom: 2em;
+        background-color: beige;
+        -webkit-transition-duration: 0.4s;  /* transition to color */
+        transition-duration: 0.4s;
+        display: block;
+    }
+    #firstButton:hover{
+        background-color: chocolate;
+        color:white
     }
 
 </style>
