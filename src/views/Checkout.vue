8a637755a8cb27f03c1bb376dc8ee38f6ce6b6ef
MrFantasio <henry.sjogren.0184@student.uu.se>
2019-12-13 14:21:27 +0100

    some changes on checkout page, popup and orderings added

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -3,6 +3,7 @@
         <button class = "backButton"><router-link to="/ordering" class="routerButton">{{uiLabels.backToOrd}}</router-link></button>
         <div class = "checkOutTable">
             <h1>Your total order: </h1>
+            <div class = "finalOrder">
             <OrderItem
                     v-for="(order, key) in this.orders"
                     v-if="order.status !== 'done'"
@@ -12,7 +13,13 @@
                     :lang="lang"
                     :key="key">
             </OrderItem>
-            <button class ="paymentButton">Ready for payment</button>
+            </div><br><br>
+            {{uiLabels.tally}}: {{this.price}} kr<br>
+            <!--<button class ="paymentButton" v-on:click = "alertFunction()">Ready for payment</button>-->
+            <button class ="paymentButton" v-on:click = "confirmedPayment = true">Ready for payment</button>
+            <div class = "paymentBox" v-show="confirmedPayment">
+                <button class = "xButton" v-on:click="confirmedPayment=false" >X</button>
+                Follow instructions in the card terminal...</div>
         </div>
     </div>
 </template>
@@ -25,16 +32,27 @@
 
     export default {
         name: "Checkout",
+        components: {
+            Ingredient,
+            OrderItem,
+            Ordering
+        },
 
         mixins: [sharedVueStuff],
 
         data: function () { //Not that data is a function!
             return {
+                confirmedPayment: false,
                 chosenIngredients: [],
                 price: 0,
                 orderNumber: "",
                 count: 0,
                 currentCategory: 1, // Category deciding what ingredients to show
+            }
+        },
+        methods: {
+            alertFunction: function() {
+                alert("Follow the instructions in the card terminal")
             }
         }
     };
@@ -45,6 +63,7 @@
 <style scoped>
     .checkout {
         background-color: beige;
+        background-size: cover;
     }
     .backButton {
         margin-left: 5em;
@@ -61,7 +80,6 @@
         border-radius: 10px;
         background-color: #ffc74a;
         border: solid black 3px;
-
     }
     .checkOutTable {
         background-color: bisque;
@@ -75,5 +93,27 @@
     .routerButton {
         text-decoration: none;
         color: black;
+    }
+    .finalOrder {
+        border:black solid 3px;
+    }
+    .paymentBox {
+        background-color: #efff9a;
+        border: solid black 3px;
+        margin-right: 5vw;
+        margin-left: 5vw;
+        padding: 5vw;
+        position: relative;
+        text-align: center;
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+    .paymentBox button {
+        background-color: #ff0000;
+        border-radius: 50%;
+        border: solid black 2px;
+
+        position:absolute;
+        top: 0px;
+        right: 0px;
     }
 </style>
