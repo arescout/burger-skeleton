ae096dbf3eccee483e58bb7fe4e2580bc43e4e50
Oskar Areskog <osar3133@student.uu.se>
2019-12-19 23:19:38 +0100

    Trying to figure out how server works

--- src/components/Ingredient.vue
+++ src/components/Ingredient.vue
@@ -73,8 +73,6 @@
                 } else {
                     this.item.currentOrderCounter = 1;
                 }
-                console.log(this.$parent.breadChosen);
-                console.log(this.$parent.currentCategory);
 
                 // sending 'increment' message to parent component or view so that it
                 // can catch it with v-on:increment in the component declaration
