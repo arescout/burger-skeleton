baf9ac7d9a0af3a8fcf80e43dda358deb09a8552
mandushjelm <mandus.hjelm.9176@student.uu.se>
2019-12-26 19:11:19 +0100

    Löst bugg med addToOrder button och checkout button.

--- src/views/Startpage.vue
+++ src/views/Startpage.vue
@@ -1,8 +1,8 @@
 <template>
     <div id = "startPage">
     <div class = "IntroText">
-        <button class="routerButton" v-on:click="switchLang()"><img src=https://upload.wikimedia.org/wikipedia/commons/4/4c/Flag_of_Sweden.svg width=20px>
-            {{uiLabels.language }}</button>
+        <button class="routerButton" v-on:click="switchLang()"><img src= width=20px>
+            {{uiLabels.language }} {{uiLabels.flagOnButton }} </button>
     <div class="welcomeText">{{uiLabels.welcomeShort}}</div>
         <section class = "welcomeSection">
             <div class="whereToEat" >
@@ -31,7 +31,7 @@
         computed: {
             eatHere: function () {
                 return this.$store.state.eatHere;
-            }
+            },
         },
         created: function () {
             this.$store.state.socket.on('orderNumber', function (data) {
